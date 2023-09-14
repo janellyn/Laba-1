dde52ba55db21e2c5ed4fdbb9efa9908853762d7
janellyn <zzheglovazhenia@gmail.com>
2023-09-14 20:32:38 +0300

    Add Main method

---
+++ Laba1/Laba1/MyLinkedList.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MyLinkedList<T> : IEnumerable<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+    private int count;
+
+    public MyLinkedList()
+    {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    public int Count => count;
+
+    private class Node<TNode>
+    {
+        public TNode Data { get; }
+        public Node<TNode> Next { get; set; }
+
+        public Node(TNode data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+    public void Add(T item)
+    {
+        Node<T> newNode = new Node<T>(item);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+            tail.Next = head;
+        }
+        else
+        {
+            newNode.Next = head;
+            tail.Next = newNode;
+            tail = newNode;
+        }
+        count++;
+        OnItemAdded(item);
+    }
+
+    public bool Remove(T item)
+    {
+        if (head == null)
+            return false;
+
+        Node<T> current = head;
+        Node<T> previous = null;
+
+        do
+        {
+            if (current.Data.Equals(item))
+            {
+                if (previous != null)
+                {
+                    previous.Next = current.Next;
+                    if (current == head)
+                        head = current.Next;
+                    if (current == tail)
+                        tail = previous;
+                }
+                else
+                {
+                    head = head.Next;
+                    tail.Next = head;
+                }
+                count--;
+                OnItemRemoved(item);
+                return true;
+            }
+
+            previous = current;
+            current = current.Next;
+        } while (current != head);
+
+        return false;
+    }
+
+    public bool Contains(T item)
+    {
+        if (head == null)
+            return false;
+
+        Node<T> current = head;
+
+        do
+        {
+            if (current.Data.Equals(item))
+                return true;
+
+            current = current.Next;
+        } while (current != head);
+
+        return false;
+    }
+
+    public event Action<T> ItemAdded;
+
+    public event Action<T> ItemRemoved;
+
+    protected virtual void OnItemAdded(T item)
+    {
+        ItemAdded?.Invoke(item);
+    }
+
+    protected virtual void OnItemRemoved(T item)
+    {
+        ItemRemoved?.Invoke(item);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        if (head != null)
+        {
+            Node<T> current = head;
+            do
+            {
+                yield return current.Data;
+                current = current.Next;
+            } while (current != head);
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
--- Laba1/Laba1/Program.cs
+++ Laba1/Laba1/Program.cs
@@ -1,136 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
-public class MyLinkedList<T> : IEnumerable<T>
+class Program
 {
-    private Node<T> head;
-    private Node<T> tail;
-    private int count;
-
-    public MyLinkedList()
+    static void Main()
     {
-        head = null;
-        tail = null;
-        count = 0;
-    }
+        MyLinkedList<int> circularList = new MyLinkedList<int>();
 
-    public int Count => count;
+        circularList.ItemAdded += item => Console.WriteLine($"Елемент {item} доданий до списку.");
+        circularList.ItemRemoved += item => Console.WriteLine($"Елемент {item} видалений зі списку.");
 
-    private class Node<TNode>
-    {
-        public TNode Data { get; }
-        public Node<TNode> Next { get; set; }
+        circularList.Add(1);
+        circularList.Add(2);
+        circularList.Add(3);
+        circularList.Remove(2);
 
-        public Node(TNode data)
+        foreach (var item in circularList)
         {
-            Data = data;
-            Next = null;
+            Console.WriteLine(item);
         }
-    }
-    public void Add(T item)
-    {
-        Node<T> newNode = new Node<T>(item);
-        if (head == null)
-        {
-            head = newNode;
-            tail = newNode;
-            tail.Next = head;
-        }
-        else
-        {
-            newNode.Next = head;
-            tail.Next = newNode;
-            tail = newNode;
-        }
-        count++;
-        OnItemAdded(item);
-    }
-
-    public bool Remove(T item)
-    {
-        if (head == null)
-            return false;
-
-        Node<T> current = head;
-        Node<T> previous = null;
-
-        do
-        {
-            if (current.Data.Equals(item))
-            {
-                if (previous != null)
-                {
-                    previous.Next = current.Next;
-                    if (current == head)
-                        head = current.Next;
-                    if (current == tail)
-                        tail = previous;
-                }
-                else
-                {
-                    head = head.Next;
-                    tail.Next = head;
-                }
-                count--;
-                OnItemRemoved(item);
-                return true;
-            }
-
-            previous = current;
-            current = current.Next;
-        } while (current != head);
-
-        return false;
-    }
-
-    public bool Contains(T item)
-    {
-        if (head == null)
-            return false;
-
-        Node<T> current = head;
-
-        do
-        {
-            if (current.Data.Equals(item))
-                return true;
-
-            current = current.Next;
-        } while (current != head);
-
-        return false;
-    }
-
-    public event Action<T> ItemAdded;
-
-    public event Action<T> ItemRemoved;
-
-    protected virtual void OnItemAdded(T item)
-    {
-        ItemAdded?.Invoke(item);
-    }
-
-    protected virtual void OnItemRemoved(T item)
-    {
-        ItemRemoved?.Invoke(item);
-    }
-
-    public IEnumerator<T> GetEnumerator()
-    {
-        if (head != null)
-        {
-            Node<T> current = head;
-            do
-            {
-                yield return current.Data;
-                current = current.Next;
-            } while (current != head);
-        }
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
     }
 }
  
