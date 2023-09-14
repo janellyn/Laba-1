0d9c40b301a458891973662d1893e1f6f2b2952e
janellyn <zzheglovazhenia@gmail.com>
2023-09-14 17:55:02 +0300

    Create MyLinkedList class

--- Laba1/Laba1/Program.cs
+++ Laba1/Laba1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+public class MyLinkedList<T> 
+{
+    private Node<T> head;
+    private Node<T> tail;
+    private int count;
+
+    public MyLinkedList()
+    {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    public int Count => count;
+
+    private class Node<TNode>
+    {
+        public TNode Data { get; }
+        public Node<TNode> Next { get; set; }
+
+        public Node(TNode data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
+ 
