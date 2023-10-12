d30d4565cd6bcdf02909d2d38e95dad8b5a9ca0e
janellyn <zzheglovazhenia@gmail.com>
2023-10-12 19:50:38 +0300

    Add two tests

--- Laba1/XUnitTests/Tests.cs
+++ Laba1/XUnitTests/Tests.cs
@@ -9,13 +9,37 @@ namespace XUnitTests
         [InlineData(2.1)]
         [InlineData(33)]
         [InlineData(1067 / 2)]
-        public void ListAddedElement(int value)
+        public void AddedElement(int value)
         {
             MyLinkedList<int> list = new MyLinkedList<int>();
             list.Add(value);
             Assert.Contains(value, list);
         }
 
+        [Theory]
+        [InlineData(-22)]
+        [InlineData(0)]
+        [InlineData(4)]
+        [InlineData(66)]
+        [InlineData(103)]
+        public void RemovedElement(int value)
+        {
+            MyLinkedList<int> list = new MyLinkedList<int>() { -22, 0, 4, 66 };
+            list.Remove(value);
+            Assert.DoesNotContain(value, list);
+        }
 
+        [Theory]
+        [InlineData(-22)]
+        [InlineData(0)]
+        [InlineData(4)]
+        [InlineData(66)]
+        [InlineData(103)]
+        public void ContainsElement(int value)
+        {
+            MyLinkedList<int> list = new MyLinkedList<int>() { -22, 0, 4, 66 };
+            bool result = list.Contains(value); 
+            Assert.Equal(result, list.Contains(value));
+        }
     }
 }
